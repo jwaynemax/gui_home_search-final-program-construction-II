2962b53458e7aa83498bac8253406be6fd745608
jm00724 <jm00724@csvlab-vm26.uwg.westga.edu>
2023-04-30 15:28:50 -0400

    init commit

---
+++ src/edu/westga/cs6312/Main.java
@@ -0,0 +1,21 @@
+package edu.westga.cs6312;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World.");
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+/**
+ * @author jm00724
+ *
+ */
+module MaxwellFinalCs6312 {
+	
+	requires javafx.graphics;
+	requires javafx.controls;
+	
+	opens edu.westga.cs6312 to javafx.graphics;
+}
