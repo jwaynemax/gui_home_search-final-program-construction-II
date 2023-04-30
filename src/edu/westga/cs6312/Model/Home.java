be491b10b42a9054ef5182895370cf6869b5ac2a
jm00724 <jm00724@csvlab-vm26.uwg.westga.edu>
2023-04-30 15:42:23 -0400

    Packages created - Main, Home, DataService classes created

---
+++ src/edu/westga/cs6312/Model/Home.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package edu.westga.cs6312.Model;
+
+/**
+ * @author jm00724
+ *
+ */
+public class Home {
+
+}
---
+++ src/edu/westga/cs6312/Service/DataService.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package edu.westga.cs6312.Service;
+
+/**
+ * @author jm00724
+ *
+ */
+public class DataService {
+
+}
